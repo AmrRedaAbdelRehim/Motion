c6706abca899e36682cbac1c77d1ea76d3771206
AmrRedaAbdelRehim <149485418+AmrRedaAbdelRehim@users.noreply.github.com>
2023-10-31 21:01:06 +0200

    Update motion.h

--- motion.h
+++ motion.h
@@ -13,17 +13,11 @@ public:
   void left();
   void stop();
   int Speed;
-  int motor_speed_1;
-  int motor_speed_2;
-  int motor_speed_3;
-  int motor_speed_4;
+  
 
 
   Motion() {
-    this->motor_speed_1 = 0;
-    this->motor_speed_2 = 0;
-    this->motor_speed_3 = 0;
-    this->motor_speed_4 = 0;
+    
     this->Speed = 80;
   };
   
